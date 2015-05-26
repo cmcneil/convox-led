7e01bbf5af1b5cfc40182011aa8f838edbe8636f
Carson McNeil <carson.mcneil@gmail.com>
2015-05-26 06:15:45 +0000

    modified test code

--- LightServer/light_machine.go
+++ LightServer/light_machine.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-  "fmt"
-  "math"
-  "time"
+	"fmt"
+	"math"
+	"time"
 )
 
 // This file contains LightMachine, which is the state machine
@@ -11,122 +11,123 @@ import (
 // a LightMachine, which tracks the state of the light progression, and can
 // transform that state into an appropriate buffer.
 type LightMachine struct {
-  //offset float64 // Offset of the first light projected onto the color wheel.
-  delta float64 // Space between two lights projected on the color wheel.
-  period uint32 // Time it takes for a light to change colors(ms)
-  nsteps uint32 // Number of distinct frames it takes a light to change colors.
-  colors [][3]byte // Colors in the wheel.
-  t0 time.Time // Time of initialization. TODO(cmcneil): worry about floating point
-          // precision later
+	//offset float64 // Offset of the first light projected onto the color wheel.
+	delta  float64   // Space between two lights projected on the color wheel.
+	period uint32    // Time it takes for a light to change colors(ms)
+	nsteps uint32    // Number of distinct frames it takes a light to change colors.
+	colors [][3]byte // Colors in the wheel.
+	t0     time.Time // Time of initialization. TODO(cmcneil): worry about floating point
+	// precision later
 }
 
-func machineFromConfig(config *ConvoxLightConfig) (*LightMachine) {
-  lm := new(LightMachine)
-  colors := config.GetColors()
+func machineFromConfig(config *ConvoxLightConfig) *LightMachine {
+	lm := new(LightMachine)
+	colors := config.GetColors()
 
-  lm.delta = float64(len(colors)) * float64(config.GetCircleCompression()) /
-              float64(nglobes)
-  //lm.offset = 0.0
-  lm.period = config.GetPeriod()
-  lm.nsteps = config.GetTransitionSteps()
-  lm.t0 = time.Now()
+	lm.delta = float64(len(colors)) * float64(config.GetCircleCompression()) /
+		float64(nglobes)
+	//lm.offset = 0.0
+	lm.period = config.GetPeriod()
+	lm.nsteps = config.GetTransitionSteps()
+	lm.t0 = time.Now()
 
-  for _, color := range colors {
-    lm.colors = append(lm.colors, colorToBytes(color))
-  }
-  // Add the first color on the end again, so the modular arithmetic works out.
-  lm.colors = append(lm.colors, lm.colors[0])
-  return lm
+	for _, color := range colors {
+		lm.colors = append(lm.colors, colorToBytes(color))
+	}
+	// Add the first color on the end again, so the modular arithmetic works out.
+	lm.colors = append(lm.colors, lm.colors[0])
+	return lm
 }
 
-func testMachine() (*LightMachine) {
-  lm := new(LightMachine)
-  lm.delta = 0.5
-  lm.nsteps = 20
-  lm.t0 = time.Now()
-  lm.period = 2000
-  lm.colors = append(lm.colors, [3]byte{150, 150, 150})
-  lm.colors = append(lm.colors, [3]byte{250, 50, 0})
-  lm.colors = append(lm.colors, [3]byte{0, 0, 0})
-  lm.colors = append(lm.colors, [3]byte{0, 200, 0})
-  lm.colors = append(lm.colors, [3]byte{150, 150, 150})
-  return lm
+func testMachine() *LightMachine {
+	lm := new(LightMachine)
+	lm.delta = 0.5
+	lm.nsteps = 20
+	lm.t0 = time.Now()
+	lm.period = 2000
+	lm.colors = append(lm.colors, [3]byte{150, 150, 150})
+	lm.colors = append(lm.colors, [3]byte{250, 50, 0})
+	lm.colors = append(lm.colors, [3]byte{0, 0, 0})
+	lm.colors = append(lm.colors, [3]byte{0, 200, 0})
+	lm.colors = append(lm.colors, [3]byte{150, 150, 150})
+	return lm
 }
 
 func (lm *LightMachine) GetBuffer() []byte {
-  tdiff := time.Since(lm.t0).Nanoseconds() / 1000000
-  o := math.Trunc(float64(tdiff) / float64(lm.period) * float64(lm.nsteps))
-  offset := o / float64(lm.nsteps)
-  buf := make([]byte, nleds * 3)
-  for i := 0; i < nglobes; i++ {
-    osGlobe := math.Mod(offset + lm.delta*float64(i),
-                        float64(len(lm.colors) - 1))
-    // fmt.Println("The globe offset: ", osGlobe)
-    // fmt.Println("Number of colors: ", len(lm.colors))
-    colGlobe := colorInterp(lm.colors[int(math.Floor(osGlobe))],
-                            lm.colors[int(math.Ceil(osGlobe))],
-                            math.Mod(osGlobe, 1.0))
-    for j := 0; j < nledsPerGlobe; j++ {
-      for k := 0; k < 3; k++ {
-        buf[i*nledsPerGlobe*3 + j*3 + k] = colGlobe[k]
-      }
-    }
-  }
-  return buf
+	tdiff := time.Since(lm.t0).Nanoseconds() / 1000000
+	o := math.Trunc(float64(tdiff) / float64(lm.period) * float64(lm.nsteps))
+	offset := o / float64(lm.nsteps)
+	buf := make([]byte, nleds*3)
+	for i := 0; i < nglobes; i++ {
+		osGlobe := math.Mod(offset+lm.delta*float64(i),
+			float64(len(lm.colors)-1))
+		// fmt.Println("The globe offset: ", osGlobe)
+		// fmt.Println("Number of colors: ", len(lm.colors))
+		colGlobe := colorInterp(lm.colors[int(math.Floor(osGlobe))],
+			lm.colors[int(math.Ceil(osGlobe))],
+			math.Mod(osGlobe, 1.0))
+		for j := 0; j < nledsPerGlobe; j++ {
+			for k := 0; k < 3; k++ {
+				buf[i*nledsPerGlobe*3+j*3+k] = colGlobe[k]
+			}
+		}
+	}
+	fmt.Println(len(buf))
+	return buf
 }
 
 // Math util functions.
 func colorToBytes(c *ConvoxLightConfig_Color) [3]byte {
-  coords := c.GetCoordinates()
-  space := c.GetColorSpace()
-  if len(coords) != 3 {
-    fmt.Print("Malformed Packet!")
-    return [3]byte{0, 0, 0}
-  }
-  var r, g, b uint32
-  switch space {
-    case ConvoxLightConfig_Color_HSV:
-      rgb := hsvToRgb([3]uint32{coords[0], coords[1], coords[2]})
-      r, g, b = rgb[0], rgb[1], rgb[2]
-    default:
-      r, g, b = coords[0], coords[1], coords[2]
+	coords := c.GetCoordinates()
+	space := c.GetColorSpace()
+	if len(coords) != 3 {
+		fmt.Print("Malformed Packet!")
+		return [3]byte{0, 0, 0}
+	}
+	var r, g, b uint32
+	switch space {
+	case ConvoxLightConfig_Color_HSV:
+		rgb := hsvToRgb([3]uint32{coords[0], coords[1], coords[2]})
+		r, g, b = rgb[0], rgb[1], rgb[2]
+	default:
+		r, g, b = coords[0], coords[1], coords[2]
 
-  }
-  return [3]byte{byte(r), byte(g), byte(b)}
+	}
+	return [3]byte{byte(r), byte(g), byte(b)}
 }
 
 // Interpolate between two colors, given the first color c1,
 // the second color, c2, and how far you are between them on a scale of 0-1.
 func colorInterp(c1 [3]byte, c2 [3]byte, t float64) [3]byte {
-  var newColor [3]byte
-  for i := range c1 {
-    newColor[i] = byte(float64(c1[i]) + (float64(c2[i]) - float64(c1[i]))*t)
-  }
-  return newColor
+	var newColor [3]byte
+	for i := range c1 {
+		newColor[i] = byte(float64(c1[i]) + (float64(c2[i])-float64(c1[i]))*t)
+	}
+	return newColor
 }
 
 func hsvToRgb(hsv [3]uint32) [3]uint32 {
-  h, s, v := float64(hsv[0]), float64(hsv[1]), float64(hsv[2])
-  c := s * v / 65536.0
-  h0 := float64(6.0 * h / 256.0)
-  x := c * float64(1.0 - math.Abs(math.Mod(h0, 2.0) - 1.0))
-  var r, g, b float64
-  if h0 < 1 {
-    r, g, b = c, x, 0
-  } else if h0 < 2 {
-    r, g, b = x, c, 0
-  } else if h0 < 3 {
-    r, g, b = 0, c, x
-  } else if h0 < 4 {
-    r, g, b = 0, x, c
-  } else if h0 < 5 {
-    r, g, b = x, 0, c
-  } else {
-    r, g, b = c, 0, x
-  }
-  m := (v / 256.0) - c
-  r += m
-  g += m
-  b += m
-  return [3]uint32{uint32(255 * r), uint32(255 * g), uint32(255 * b)}
+	h, s, v := float64(hsv[0]), float64(hsv[1]), float64(hsv[2])
+	c := s * v / 65536.0
+	h0 := float64(6.0 * h / 256.0)
+	x := c * float64(1.0-math.Abs(math.Mod(h0, 2.0)-1.0))
+	var r, g, b float64
+	if h0 < 1 {
+		r, g, b = c, x, 0
+	} else if h0 < 2 {
+		r, g, b = x, c, 0
+	} else if h0 < 3 {
+		r, g, b = 0, c, x
+	} else if h0 < 4 {
+		r, g, b = 0, x, c
+	} else if h0 < 5 {
+		r, g, b = x, 0, c
+	} else {
+		r, g, b = c, 0, x
+	}
+	m := (v / 256.0) - c
+	r += m
+	g += m
+	b += m
+	return [3]uint32{uint32(255 * r), uint32(255 * g), uint32(255 * b)}
 }
--- LightServer/light_server.go
+++ LightServer/light_server.go
@@ -1,50 +1,52 @@
 package main
 
 import (
-  "fmt"
-  "github.com/golang/protobuf/proto"
-  "net"
-  "time"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"net"
+	"time"
 )
 
 func startServer() {
-  // Bind the port.
-  ServerAddr, err := net.ResolveUDPAddr("udp", "localhost:666")
-  if (err != nil) {
-    fmt.Println("Error binding port!")
-  }
-  fmt.Println("Beginning Convox LightServer!")
-  fmt.Println(ServerAddr)
+	// Bind the port.
+	ServerAddr, err := net.ResolveUDPAddr("udp", "localhost:666")
+	if err != nil {
+		fmt.Println("Error binding port!")
+	}
+	fmt.Println("Beginning Convox LightServer!")
+	fmt.Println(ServerAddr)
 
-  ServerConn, _ := net.ListenUDP("udp", ServerAddr)
-  defer ServerConn.Close()
+	ServerConn, _ := net.ListenUDP("udp", ServerAddr)
+	defer ServerConn.Close()
 
-  pushConf := LightManager()
+	pushConf := LightManager()
 
-  buf := make([]byte, 1024)
-  for {
-    // Recieve a UDP packet and unmarshal it into a protobuf.
-    n, _, _ := ServerConn.ReadFromUDP(buf)
-    indata := new(ConvoxLightConfig)
-    marshalerr := proto.Unmarshal(buf[0:n], indata)
-    if (marshalerr != nil) {
-      fmt.Print("Corrupt packet!")
-      continue
-    }
+	buf := make([]byte, 1024)
+	for {
+		// Recieve a UDP packet and unmarshal it into a protobuf.
+		n, _, _ := ServerConn.ReadFromUDP(buf)
+		indata := new(ConvoxLightConfig)
+		marshalerr := proto.Unmarshal(buf[0:n], indata)
+		if marshalerr != nil {
+			fmt.Print("Corrupt packet!")
+			continue
+		}
 
-    //fmt.Print(indata)
-    pushConf(indata)
-    /* Do stuff with indata. */
-  }
+		//fmt.Print(indata)
+		pushConf(indata)
+		/* Do stuff with indata. */
+	}
 }
 
 func main() {
-  // startServer()
-  lm := testMachine()
-  datachan := make(chan []byte)
-  go writer(datachan)
-  for {
-    time.Sleep(100 * time.Millisecond)
-    datachan<-lm.GetBuffer()
-  }
+	// startServer()
+	lm := testMachine()
+	datachan := make(chan []byte)
+	go writer(datachan)
+	for {
+		time.Sleep(1000 * time.Millisecond)
+		buf := lm.GetBuffer()
+		fmt.Print(buf)
+		datachan <- lm.GetBuffer()
+	}
 }
